535904f6ecffd8fb8750aba38c711382b695a540
sauravshub1 <sauravshub1@gmail.com>
2020-09-15 16:27:01 +0530

    Updates

---
+++ precache-manifest.91dabe67669d9a872691f60d30c44c68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dabf2de7c34c0dda65cf51359d045d2c",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "5c17c9129f2e2364456b",
+    "url": "/Calculator/static/css/main.0a50f982.chunk.css"
+  },
+  {
+    "revision": "46514c20b3edc2e9cc5e",
+    "url": "/Calculator/static/js/2.4ae3a3f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.4ae3a3f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c17c9129f2e2364456b",
+    "url": "/Calculator/static/js/main.82b5465b.chunk.js"
+  },
+  {
+    "revision": "0a79fa1d2af7875576c8",
+    "url": "/Calculator/static/js/runtime-main.8483c221.js"
+  }
+]);
